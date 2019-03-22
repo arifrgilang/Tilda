c17f9c69f731ebc2f4b2f15a6d951fd6c4a07520
Arif R Gilang <arif17003@mail.unpad.ac.id>
2019-03-23 03:50:22 +0700

    add result and alert dialog

--- app/src/main/java/com/rz/tilda/CustomTabs.kt
+++ app/src/main/java/com/rz/tilda/CustomTabs.kt
@@ -37,7 +37,7 @@ object CustomTabs{
         )
 
         customTabsIntent = CustomTabsIntent.Builder(mCustomTabsSession)
-            .setToolbarColor(ContextCompat.getColor(activity, R.color.colorPrimaryDark))
+            .setToolbarColor(ContextCompat.getColor(activity, R.color.colorPrimary))
             .enableUrlBarHiding()
             .build()
         customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
